a8ccc5affa93c1a57e53790a2d8e6bd3a7eb03f4
arsad <isaarsad0@gmail.com>
2026-06-17 14:59:38 +0700

    finished documentations

--- src/Infrastructures/database/postgres/seed.js
+++ src/Infrastructures/database/postgres/seed.js
@@ -3,27 +3,16 @@ async function seed() {
   try {
     console.log('=== Memulai Proses Seeding Database ===');
 
-    await pool.query('TRUNCATE role_menu_access, menus, user_roles, roles, users CASCADE;');
-
-    await pool.query(`
-      INSERT INTO users (id, username, password, fullname) VALUES 
-      ('user-123', 'arsad', '$2b$10$76YVwGz1mXb7yXwG7uR8e.ZfK5bW4eE6aO7gY3xN9zH2.mK5l8uSq', 'Isa Arsad');
-    `);
-    console.log('✓ User "arsad" berhasil ditambahkan.');
+    await pool.query(
+      'TRUNCATE role_menu_access, menus, user_roles, roles, users, authentications CASCADE;',
+    );
+    console.log('✓ TRUNCATE all tables.');
 
     await pool.query(`
       INSERT INTO roles (id, role_name) VALUES 
-      ('role-123', 'Administrator'),
-      ('role-456', 'Staff Operational');
+      ('role-123', 'Administrator')
     `);
     console.log('✓ Roles berhasil ditambahkan.');
-
-    await pool.query(`
-      INSERT INTO user_roles (user_id, role_id) VALUES 
-      ('user-123', 'role-123'),
-      ('user-123', 'role-456');
-    `);
-    console.log('✓ Jabatan ganda berhasil di-set untuk user.');
 
     await pool.query(`
       INSERT INTO menus (id, menu_name, url, parent_id, order_index) VALUES 
@@ -50,19 +39,6 @@ async function seed() {
       ('m3.2', 'Menu 3.2', '/menu-3/2', 'm3', 2);
     `);
     console.log('✓ Struktur menu berlevel berhasil ditambahkan.');
-
-    await pool.query(`
-      INSERT INTO role_menu_access (role_id, menu_id) 
-      SELECT 'role-123', id FROM menus;
-    `);
-
-    await pool.query(`
-      INSERT INTO role_menu_access (role_id, menu_id) VALUES 
-      ('role-456', 'm3'),
-      ('role-456', 'm3.1'),
-      ('role-456', 'm3.2');
-    `);
-    console.log('✓ Hak akses role_menu_access berhasil di-set.');
 
     console.log('=== Seeding Selesai ===');
   } catch (error) {
